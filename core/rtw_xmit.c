69315f55212d164452de87392e358880524c0d3e
Sophie Brun <sophie@offensive-security.com>
2020-09-16 09:55:38 +0200

    New upstream version 5.6.4.2~git20200916

--- core/rtw_xmit.c
+++ core/rtw_xmit.c
@@ -2017,7 +2017,9 @@ s32 rtw_txframes_sta_ac_pending(_adapter *padapter, struct pkt_attrib *pattrib)
 
 	}
 
-	return ptxservq->qcnt;
+	if (ptxservq)
+		return ptxservq->qcnt;
+	return 0;
 }
 
 #ifdef CONFIG_TDLS
@@ -4460,6 +4462,7 @@ s32 rtw_monitor_xmit_entry(struct sk_buff *skb, struct net_device *ndev)
 	/* Check DATA/MGNT frames */
 	pwlanhdr = (struct rtw_ieee80211_hdr *)pframe;
 	pattrib = &pmgntframe->attrib;
+	pattrib->injected = _TRUE;
 
 	if (pregpriv->monitor_disable_1m) {
 
@@ -4484,7 +4487,11 @@ s32 rtw_monitor_xmit_entry(struct sk_buff *skb, struct net_device *ndev)
 		pattrib->stbc = 0;
 
 	}
-	pattrib->retry_ctrl = _FALSE;
+
+	if (pregpriv->monitor_retransmit)
+		pattrib->retry_ctrl = _TRUE;
+	else
+		pattrib->retry_ctrl = _FALSE;
 	pattrib->pktlen = len;
 	pmlmeext->mgnt_seq = GetSequence(pwlanhdr);
 	pattrib->seqnum = pmlmeext->mgnt_seq;
